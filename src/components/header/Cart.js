eadf2bbdeb33160e2022c6cddba746182e315959
Nazir Hussain <56648155+nazir-hussain@users.noreply.github.com>
2023-01-11 15:04:19 +0530

    Update Cart.js

--- src/components/header/Cart.js
+++ src/components/header/Cart.js
@@ -3,7 +3,6 @@ import Icon from '@mdi/react';
 import { mdiCartOutline } from '@mdi/js';
 import { Link } from 'react-router-dom';
 import './Cart.scss';
-
 import { useCartState } from 'cart-context';
 
 function Cart() {
