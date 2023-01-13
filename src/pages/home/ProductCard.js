0af304c8d535569025e20e2505cc90fd6695d06f
Nazir Hussain <56648155+nazir-hussain@users.noreply.github.com>
2023-01-13 18:07:41 +0530

    Update ProductCard.js

--- src/pages/home/ProductCard.js
+++ src/pages/home/ProductCard.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import classNames from 'classnames';
 import { Link } from 'react-router-dom';
-
 import './ProductCard.scss';
 
 import CurrencyFormat from 'components/general/CurrencyFormat';
