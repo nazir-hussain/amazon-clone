7357ed9dac5bcdf570fd2fed582f5476c69b3e83
Nazir Hussain <56648155+nazir-hussain@users.noreply.github.com>
2023-01-15 23:17:12 +0530

    Update index.js

--- src/pages/product/index.js
+++ src/pages/product/index.js
@@ -3,7 +3,6 @@ import { useParams } from "react-router-dom";
 import ImageSlider from '../../components/product/ImageSlider';
 import Information from '../../components/product/Information';
 import './index.scss';
-
 import productsApi from 'api/products';
 import Loader from 'components/general/Loader';
 import Error from 'components/general/Error';
